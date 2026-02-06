4ddfdfa016e2f4a63287add7a485610cc79583ea
mauricionlz2018-tech <mauricionlz2018@gmail.com>
2026-02-05 18:13:15 -0600

    Prueba033

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
               </li>
               <li>
                 <Link href="/academics" className="hover:underline hover:scale-105 transition-smooth">
-                  Academico
+                  Académico
                 </Link>
               </li>
               <li>
